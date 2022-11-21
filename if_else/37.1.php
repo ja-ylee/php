f5ce07349826346553514f9d4697165bbddcce87
savinov <savinov04jays@gmail.com>
2022-11-21 07:50:39 +0000

    37.1

---
+++ if_else/37.1.php
@@ -0,0 +1,15 @@
+<?php
+$day = 10;
+
+if ($day >= 1 and $day <= 10){
+    echo '1 декада';
+}
+
+if ($day >= 11 and $day <= 20) {
+    echo '2 декада';
+}
+
+if ($day >= 21 and $day <= 31) {
+    echo '3 декада';
+}
+?>
