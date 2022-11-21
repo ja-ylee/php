f6a94979e565c1480fff643140bed7fc2378bd6c
savinov <savinov04jays@gmail.com>
2022-11-21 07:40:24 +0000

    29.7

---
+++ if_else/29.7.php
@@ -0,0 +1,11 @@
+<?php
+$test1 = 11;
+$test2 = 22;
+if ($test > $test2){
+    echo 'test1 > test2';
+}   else if ($test1 < $test2) {
+    echo 'test1 < test2';
+}   else if ($test1 == $test2) {
+    echo 'test1 = test2';
+}
+?>
