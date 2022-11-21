fd9034ea26a4afdcb7ae1da4c56d32deeb916c07
savinov <savinov04jays@gmail.com>
2022-11-21 07:52:12 +0000

    40.1

---
+++ if_else/40.1.php
@@ -0,0 +1,17 @@
+<?php
+$num = 3;
+switch ($num){
+    case 1:
+        echo '1 четверть';
+        break;
+    case 2:
+        echo '2 четверть';
+        break;
+    case 3:
+        echo '3 четверть';
+        break;
+    case 4:
+        echo '4 четверть';
+        break;
+}
+?>
